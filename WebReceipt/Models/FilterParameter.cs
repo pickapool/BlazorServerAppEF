d8bf15aa9dcffeb14f8308f5ff224c970e6d934a
Cyrille <cyrilleconm@gmail.com>
2026-05-09 17:16:00 +0800

    Upgrade to .NET 10

--- WebReceipt/Models/FilterParameter.cs
+++ WebReceipt/Models/FilterParameter.cs
@@ -8,13 +8,13 @@ namespace WebReceipt.Models
         public bool IsDate { get; set; }
         public DateRange _dateRange { get; set; } = new DateRange(DateTime.Now, DateTime.Now);
         public bool IsPayor { get; set; }
-        public string PayorName { get; set; }
-        public bool IsORNumber;
-        public string ORNumber { get; set; }
+        public string PayorName { get; set; } = string.Empty;
+        public bool IsORNumber { get; set; }
+        public string ORNumber { get; set; } = string.Empty;
         public bool IsTransactionType { get; set; }
-        public IEnumerable<Enums.TransactionType?> ListOfTransaction { get; set; } = new HashSet<Enums.TransactionType?>();
-        public IEnumerable<PaymentTypeModel?> ListOfPayments { get; set; } = new HashSet<PaymentTypeModel?>();
+        public IReadOnlyCollection<Enums.TransactionType?> ListOfTransaction { get; set; } = System.Array.Empty<Enums.TransactionType?>();
+        public IReadOnlyCollection<PaymentTypeModel> ListOfPayments { get; set; } = System.Array.Empty<PaymentTypeModel>();
         public bool IsCollector { get; set; }
-        public string CollectorName { get; set; }
+        public string CollectorName { get; set; } = string.Empty;
     }
 }
